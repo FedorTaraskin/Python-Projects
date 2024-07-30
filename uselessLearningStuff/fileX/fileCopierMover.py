8d7c2ea7138c8c559abd524dd1a289084946fa6b
Fedor <FedorTaraskin@protonmail.com>
2024-07-30 19:51:01 +0200

    Made cleaner and more optimized: though untested

--- uselessLearningStuff/fileX/fileCopierMover.py
+++ uselessLearningStuff/fileX/fileCopierMover.py
@@ -23,20 +23,20 @@ if os.path.exists(src):
 			from shutil import copy
 			copy(src, dest)
 			print(f'The file from {src} has been copied to {dest}.')
-		elif mode == 'm':
-			os.replace(src, dest)
-			print(f'The file from {src} has been moved to {dest}.')
 			
 	elif os.path.isdir(src):
 		if mode == 'c':
 			from shutil import copytree
 			copytree(src, dest)
 			print(f'The folder from {src} has been copied to {dest}.')
-		elif mode == 'm':
+
+	##This bit of code has been moved out of [elif os.path.is[dir/file](src):] because os.replace works for files and DIRs.
+	##shutil.copy(src, dest) only works for files, and copytree(src, dest) for DIRs.
+	if mode == 'm':
 			os.replace(src, dest)
-			print(f'The folder from {src} has been moved to {dest}.')
+			print(f'The file from {src} has been moved to {dest}.')
 
 	else: print('ERROR: The source path exists, but is not a file nor a directory.')
-	
+
 elif not os.path.exists(src): print ('Sorry, this path does not exist.')
 else: print('An unexpected ERROR ocurred, please send the developer a screenshot of this program.')
